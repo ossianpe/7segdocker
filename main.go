0a8ab9fe42873be2832a28642bfd664e25e54a90
Peter Ossian <peter.ossian@ge.com>
2015-10-31 22:50:05 -0700

    Code compiles; Migrated functions to case statement

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
-# Code template taken from http://gobot.io/documentation/platforms/raspi/
+// Code template taken from http://gobot.io/documentation/platforms/raspi/
 
 package main
 
 import (
-    "time"
+    "fmt"
+    "log"
 
     "github.com/hybridgroup/gobot"
     "github.com/hybridgroup/gobot/platforms/raspi"
@@ -12,7 +13,7 @@ import (
     "github.com/samalba/dockerclient"
 )
 
-func runningContainers() {
+func runningContainers() int {
     // Init the client
     docker, _ := dockerclient.NewDockerClient("unix:///var/run/docker.sock", nil)
     // Get only running containers
@@ -20,85 +21,146 @@ func runningContainers() {
     if err != nil {
         log.Fatal(err)
     }
+    fmt.Println(len(containers))
     return len(containers)
 }
 
 func main() {
-    gbot := gobot.NewGobot()
-
     raspiAdaptor := raspi.NewRaspiAdaptor("raspi")
     a := gpio.NewLedDriver(raspiAdaptor, "a", "16")
     b := gpio.NewLedDriver(raspiAdaptor, "b", "12")
-    c := gpio.NewLedDriver(raspiAdaptor, "c", "16") # changed from 5 to 16
+    c := gpio.NewLedDriver(raspiAdaptor, "c", "16") // changed from 5 to 16
     d := gpio.NewLedDriver(raspiAdaptor, "d", "6")
     e := gpio.NewLedDriver(raspiAdaptor, "e", "0")
     f := gpio.NewLedDriver(raspiAdaptor, "f", "1")
     g := gpio.NewLedDriver(raspiAdaptor, "g", "24")
     dp := gpio.NewLedDriver(raspiAdaptor, "dp", "26")
 
-    zero := func() {
-        a.On()
-        b.On()
-        c.On()
-        d.On()
-        e.On()
-        f.On()
-    }
-
-    one := func() {
-        b.On()
-        c.On()
-    }
-
-    two := func() {
-        a.On()
-        b.On()
-        d.On()
-        e.On()
-        g.On()
-    }
-
-    three := func() {
-        a.On()
-        b.On()
-        c.On()
-        d.On()
-        g.On()
-    }
-
-    four := func() {
-        b.On()
-        c.On()
-    }
-
-    one := func() {
-        b.On()
-        c.On()
-    }
-
-    one := func() {
-        b.On()
-        c.On()
-    }
-
-    one := func() {
-        b.On()
-        c.On()
-    }
-
-    one := func() {
-        b.On()
-        c.On()
-    }
-
-    one := func() {
-        b.On()
-        c.On()
+    work := func() {
+        containerCount := runningContainers()
+//        fmt.Println(containerCount)
+        switch containerCount {
+          case 0:
+            a.On()
+            b.On()
+            c.On()
+            d.On()
+            e.On()
+            f.On()
+            g.Off()
+            dp.On()
+            return
+          case 1:
+            a.Off()
+            b.On()
+            c.On()
+            d.Off()
+            e.Off()
+            f.Off()
+            g.Off()
+            dp.On()
+            return
+          case '2':
+            a.On()
+            b.On()
+            c.Off()
+            d.On()
+            e.On()
+            f.Off()
+            g.On()
+            dp.On()
+            return
+          case '3':
+            a.On()
+            b.On()
+            c.On()
+            d.On()
+            e.Off()
+            f.Off()
+            g.On()
+            dp.On()
+            return
+          case '4':
+            a.Off()
+            b.On()
+            c.On()
+            d.Off()
+            e.Off()
+            f.Off()
+            g.Off()
+            dp.On()
+            return
+          case '5':
+            a.Off()
+            b.On()
+            c.On()
+            d.Off()
+            e.Off()
+            f.Off()
+            g.Off()
+            dp.On()
+            return
+          case '6':
+            a.Off()
+            b.On()
+            c.On()
+            d.Off()
+            e.Off()
+            f.Off()
+            g.Off()
+            dp.On()
+            return
+          case '7':
+            a.Off()
+            b.On()
+            c.On()
+            d.Off()
+            e.Off()
+            f.Off()
+            g.Off()
+            dp.On()
+            return
+          case '8':
+            a.Off()
+            b.On()
+            c.On()
+            d.Off()
+            e.Off()
+            f.Off()
+            g.Off()
+            dp.On()
+            return
+          case '9':
+            a.Off()
+            b.On()
+            c.On()
+            d.Off()
+            e.Off()
+            f.Off()
+            g.Off()
+            dp.On()
+            return
+          default:
+            a.On()
+            b.On()
+            c.On()
+            d.On()
+            e.On()
+            f.On()
+            g.On()
+            dp.On()
+        }
     }
 
     robot := gobot.NewRobot("bot",
         []gobot.Connection{raspiAdaptor},
         []gobot.Device{a},
+        []gobot.Device{b},
+        []gobot.Device{c},
+        []gobot.Device{d},
+        []gobot.Device{e},
+        []gobot.Device{f},
+        []gobot.Device{g},
         work,
     )
 
